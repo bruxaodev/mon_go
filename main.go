6de942634c9ec3159e10b4f8d4e8cda821ac5daf
bruxaodev <bruxaogamedev96@gmail.com>
2024-12-19 19:09:57 -0300

    first

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var db *mongo.Database
+
+func MongoConnect(uri, database string) error {
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		return err
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+
+	db = client.Database(database)
+	return nil
+}
+
+func GetDatabase() *mongo.Database {
+	return db
+}
+
+func Close() error {
+	return db.Client().Disconnect(context.Background())
+}
